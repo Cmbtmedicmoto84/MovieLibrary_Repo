a3b2cea8984cc40b2b731bdc9a7efdf75d3b401c
Steven Morimoto <smorimoto2284@gmail.com>
2020-06-04 10:17:42 -0500

    working on the 'get' for individual movie in order to have the option of editing said movie.

--- Client/app.js
+++ Client/app.js
@@ -43,6 +43,25 @@ function getAllMovies(){
     });
 }
 
+
+function getMovieById(){
+    $("getMovieById").html("");
+    $.ajax({
+        url: "https://localhost:44325/api/movie",
+        contentType: 'application/json',
+        success: function( data, textStatus, jQxhr ){
+            console.log(data);
+            for(let i = 0; i < data.length; i++){
+                $("getMovieById").append(`<p>Title: ${data[0]["title"]}, Director: ${data[0]["director"]}, Genre: ${data[0]["genre"]} </p>`);
+            }
+            
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log(errorThrown);
+        }
+    });
+}
+
 function jqMovieInput() {
     let valueofInput = $("#Input").val();
 
